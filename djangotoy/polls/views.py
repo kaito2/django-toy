c41c530417f3e9a10b6f04c6ccc4bdda4805d9e2
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-02-29 16:28:38 +0900

    A shortcut: get_object_or_404()

--- djangotoy/polls/views.py
+++ djangotoy/polls/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Hello, world. You are at the polls index.")
+from .models import Question
 
 
+def index(request: HttpRequest) -> HttpResponse:
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request: HttpRequest, question_id: str) -> HttpResponse:
+    # TODO: learn how to execute.
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', question)
+
+
+def results(request: HttpRequest, question_id: str) -> HttpResponse:
+    return HttpResponse(
+        f"You're looking at the results of question {question_id}.")
+
+
+def vote(request: HttpRequest, question_id: str) -> HttpResponse:
+    return HttpResponse(f"You're voting on question {question_id}.")
